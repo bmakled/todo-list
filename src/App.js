7e31077760f3606c259f5c2f1d651672a0878e45
Basiel Makled <basiel1234@gmail.com>
2023-01-27 12:05:28 -0600

    first commit, add and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit = (e) => {
+    e.preventDefault()
+    setTodos([...todos, newTodo])
+  }
+
+  const handleTodoDelete = (deleteindex) =>{
+    const filteredTodos = todos.filter((todo,i) => {
+      return i != deleteindex
+    })
+
+    setTodos(filteredTodos)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(event)=>{handleNewTodoSubmit(event);}}>
+        <input onChange = {(event)=>{setNewTodo(event.target.value)}} type="text" value = {newTodo}/>
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+
+      {todos.map((todo,i)=>{
+        return (
+          <div key={i}>
+            <span>{todo}</span>
+            <button onClick={(event)=> {handleTodoDelete(i)}}>Delete</button>
+          </div>
+        );
+      })}
+
     </div>
   );
 }
