811298f28f8f60692e5528f2dc2f027648b97b9e
Basiel Makled <basiel1234@gmail.com>
2023-01-27 14:18:56 -0600

    added strikethrough functionallity

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useState} from "react";
 
@@ -9,31 +8,59 @@ function App() {
 
   const handleNewTodoSubmit = (e) => {
     e.preventDefault()
-    setTodos([...todos, newTodo])
+
+    if (newTodo.length === 0){
+      return;
+    }
+
+    const todoItem ={
+      text: newTodo,
+      complete: false
+    }
+
+    setTodos([...todos, todoItem])
   }
 
   const handleTodoDelete = (deleteindex) =>{
     const filteredTodos = todos.filter((todo,i) => {
-      return i != deleteindex
+      return i !== deleteindex
     })
 
     setTodos(filteredTodos)
   }
+
+  const handleToStrike = (index) => {
+    const updatedTodos = todos.map((todo,i) => {
+      if (index === i){
+        todo.complete =! todo.complete;
+      }
+      return todo;
+    })
+
+    setTodos(updatedTodos)
+  } 
 
   return (
     <div className="App">
       <form onSubmit={(event)=>{handleNewTodoSubmit(event);}}>
         <input onChange = {(event)=>{setNewTodo(event.target.value)}} type="text" value = {newTodo}/>
         <div>
-          <button>Add</button>
+          <button className='button' >Add</button>
         </div>
       </form>
 
       {todos.map((todo,i)=>{
+        const todoClasses = [];
+        if (todo.complete){
+          todoClasses.push("strike-through")
+        }
+
         return (
           <div key={i}>
-            <span>{todo}</span>
-            <button onClick={(event)=> {handleTodoDelete(i)}}>Delete</button>
+            <span className={todoClasses.join("")}>{todo.text}</span>
+            <input checked={todo.complete} type="checkbox" onChange = {(event) => {handleToStrike(i)}}/>
+            <button className='button' onClick={(event)=> {handleTodoDelete(i)}}>Delete</button>
+            
           </div>
         );
       })}
